f17321ccc0b65f02dd1bc34584dd85532316f3ec
jishnusenthilkumar <jishnusenthilkumar@gmail.com>
2025-07-22 10:41:13 +0100

    QFT works

--- nQubitQFT_decompose/test_qft.py
+++ nQubitQFT_decompose/test_qft.py
@@ -1,16 +1,17 @@
 from qiskit.quantum_info import Operator
-from qft_native import qft, to_qiskit
+from qft_native.qft import qft
+from qft_native.simulator import to_qiskit
 
 
-def test_qft_produces_circuit():
+def test_qft_produces_circuit(n):
     """Basic check that ``qft`` returns a valid circuit."""
-    n = 4
     gates = qft(n)
     circuit = to_qiskit(gates, n)
     assert circuit.num_qubits == n
     assert len(gates) > 0
     Operator(circuit)  # ensure circuit is unitary
+    print(circuit.draw())
 
 
 if __name__ == "__main__":
-    test_qft_produces_circuit()
+    test_qft_produces_circuit(3)
