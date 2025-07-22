6f3a60df03bc3650bee65e4da1d6ccf20015336b
jishnusenthilkumar <jishnusenthilkumar@gmail.com>
2025-07-22 09:55:04 +0100

    Make tests a package and fix imports

--- qft_native/tests/test_qft.py
+++ qft_native/tests/test_qft.py
@@ -1,10 +1,6 @@
-import os
-import sys
 from qiskit.quantum_info import Operator
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-from qft_native import qft, to_qiskit
+from .. import qft, to_qiskit
 
 
 def test_qft_produces_circuit(n):
